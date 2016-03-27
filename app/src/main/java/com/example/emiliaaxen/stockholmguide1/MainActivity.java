71c05f9e7ff90d0b66e32e691b0ce5c6074d7714
Em Ax <emilia.axen@gmail.com>
2016-03-27 16:49:23 -0700

    ..

--- app/src/main/java/com/example/emiliaaxen/stockholmguide1/MainActivity.java
+++ app/src/main/java/com/example/emiliaaxen/stockholmguide1/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 import com.example.emiliaaxen.stockholmguide1.model.StockholmItem;
@@ -153,19 +152,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return false;
-        //return super.onOptionsItemSelected(item);
-    }*/
 
     private boolean checkIfFirstTimeRunningApp() {
         boolean isFirstRun = sharedPref.getBoolean(PREF_KEY_FIRST_APP_RUN, true);
